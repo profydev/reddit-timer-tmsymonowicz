eb3171e09993aad602bc7b204c98b3bf52dd4458
tmsymonowicz <tmsymonowicz@gmail.com>
2022-08-15 11:06:16 +0200

    added router with routes / and /search

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <Routes>
+      <Route path="/" />
+      <Route path="/search" />
+    </Routes>
   );
 }
 
