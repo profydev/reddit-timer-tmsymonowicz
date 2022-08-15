c6cfba3129bd12f86c6e9d3ec4749895115ac9c2
tmsymonowicz <tmsymonowicz@gmail.com>
2022-08-15 11:24:32 +0200

    fixed router issue

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" />
-      <Route path="/search" />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" />
+        <Route path="/search" />
+      </Routes>
+    </Router>
   );
 }
 
